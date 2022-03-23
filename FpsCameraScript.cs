2b29e295c9953483d3cbf616183f92d0a7dde9b3
_Saffan_ <101597266+Florianus-Adiantum@users.noreply.github.com>
2022-03-23 23:55:40 +0300

    Update FpsCameraScript.cs

--- FpsCameraScript.cs
+++ FpsCameraScript.cs
@@ -2,30 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KameraKontrol : MonoBehaviour
+public class FpsCameraScript : MonoBehaviour
 {
     public GameObject camera;
     
 
-    float hassasiyet = 5f;
-    float yumusaklik = 2f;
+    float sensibility = 5f;
+    float softness = 2f;
     Vector2 gecisPos;
     Vector2 camPos;
-    public GameObject oyuncu;
+    public GameObject player;
     void Start()
     {
-        oyuncu = transform.parent.gameObject;
+        player = transform.parent.gameObject;
     }
 
     
     void Update()
     {
         Vector2 farepos = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        farepos = Vector2.Scale(farepos, new Vector2(hassasiyet*yumusaklik,hassasiyet*yumusaklik));
-        gecisPos.x = Mathf.Lerp(gecisPos.x, farepos.x, 1f / yumusaklik);
-        gecisPos.y = Mathf.Lerp(gecisPos.y, farepos.y, 1f / yumusaklik);
+        farepos = Vector2.Scale(farepos, new Vector2(sensibility*softness,sensibility*softness));
+        gecisPos.x = Mathf.Lerp(gecisPos.x, farepos.x, 1f / softness);
+        gecisPos.y = Mathf.Lerp(gecisPos.y, farepos.y, 1f / softness);
         camPos += gecisPos;
         transform.localRotation = Quaternion.AngleAxis(-camPos.y, Vector3.right);
-        oyuncu.transform.localRotation = Quaternion.AngleAxis(camPos.x, oyuncu.transform.up);
+        player.transform.localRotation = Quaternion.AngleAxis(camPos.x, player.transform.up);
     }
 }
