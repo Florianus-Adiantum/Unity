f885d6cb238fc33cadedcf61892e0b0c81007838
_Saffan_ <101597266+Florianus04@users.noreply.github.com>
2022-05-28 15:26:20 +0300

    Add files via upload

---
+++ GeceGunduzDongusu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeceGunduzDongusu : MonoBehaviour
+{
+    //Gunes ve ay olmak uzere iki directional light olusturun
+    //Biri terrainin altinda digeri ustunde ve ikisi de terraine bakacak sekilde yerlestirin
+    //Gunes y ekseninde 500 degerde ise ay -500 olmali
+    //Bu kodu iki objeye de baglayin
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.RotateAround (new Vector3(500f,0,500f),Vector3.right,1f*Time.deltaTime);
+    }
+}
