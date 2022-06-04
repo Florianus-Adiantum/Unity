3b2d77494c449c67ba534fec3347e9e466c7d636
_Saffan_ <101597266+Florianus04@users.noreply.github.com>
2022-06-04 23:23:29 +0300

    Update GeceGunduzDongusu.cs

--- GeceGunduzDongusu.cs
+++ GeceGunduzDongusu.cs
@@ -6,6 +6,7 @@ public class GeceGunduzDongusu : MonoBehaviour
 {
     //Gunes ve ay olmak uzere iki directional light olusturun
     //Biri terrainin altinda digeri ustunde ve ikisi de terraine bakacak sekilde yerlestirin
+    //Yorungenin daha saglam olmasi icin gunes ve ayi hiyerarside tek bir objenin icine koyun
     //Gunes y ekseninde 500 degerde ise ay -500 olmali
     //Bu kodu iki objeye de baglayin
     void Start()
