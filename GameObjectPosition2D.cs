682257019346b8e2a487a574d688ab3908a4ddd8
_Saffan_ <101597266+Florianus04@users.noreply.github.com>
2022-06-08 21:52:20 +0300

    Update GameObjectPosition2D.cs

--- GameObjectPosition2D.cs
+++ GameObjectPosition2D.cs
@@ -15,6 +15,6 @@ public class GameObjectPosition2D : MonoBehaviour
     void Update()
     {
         Vector3 farePos=Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10f));
-        transform.position=new Vector3(farePos.x,transform.position.y,transform.position.z);s
+        transform.position=new Vector3(farePos.x,transform.position.y,transform.position.z);
     }
 }
