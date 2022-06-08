315519262f1bc64a441ecd1b8842a0000b7c4999
_Saffan_ <101597266+Florianus04@users.noreply.github.com>
2022-06-08 21:50:59 +0300

    Add files via upload

---
+++ GameObjectPosition2D.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPosition2D : MonoBehaviour
+{
+    //Bu kod 2d oyunlar icindir
+    //Bagladiginiz 2d objenin mouse ile es zamanli saga ve sola hareket etmesini saglar
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Vector3 farePos=Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10f));
+        transform.position=new Vector3(farePos.x,transform.position.y,transform.position.z);s
+    }
+}
