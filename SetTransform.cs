141cad142f699a8e1579571a0d4381852689ec9e
_Saffan_ <101597266+Florianus04@users.noreply.github.com>
2022-12-14 19:25:18 +0300

    Add files via upload

---
+++ SetTransform.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTransform : MonoBehaviour
+{
+    //bu metod istediðiniz objenin 3 eksendeki yerini kontrol etmenizi saglar
+    //metodu sürekli olarak veya belli durumlarda cok amacli kullanabilirsiniz
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        SetTransformXYZ(5, 5, 5);//3 deger icin ornek kullanim
+        SetTransformY(5);//tek deger icin ornek kullaným
+    }
+    void SetTransformXYZ(float x, float y, float z)//x y z eksenleri icin 3 float degeri alir
+    {
+        transform.position = new Vector3(x,y,z);
+    }
+    void SetTransformY(float y)//sadece y eksenini kontrol eder
+    {
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        //degistirmek istemediginiz degerlere "transform.position" ifadesi girin
+    }
+}
